ff68f3263a9edfd4ba2511b161524ae1b423b023
kowalix007 L.K <kowalix007@gmail.com>
2020-06-21 20:52:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   key_for_min_value = nil
   
-  name_hash.each_pair do |key, value|
+  name_hash.each do |key, value|
   
   if min_value == nil || value < min_value
   min_value = value
